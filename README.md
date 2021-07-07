f0c25e527323133b47d24222d16a1104cee1dd68
coelho.matheus@outlook.com <MatheusBispoDev>
2021-07-07 00:40:57 -0300

    dia-um
    primeiras anotacoes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# alura-csharp
+# Alura CSharp Courses
 Cursos do Alura para CSharp
+
+## NOTES
+
+Definitions</br>
+
+    C# \ VB \ F# \ ETC
+
+    Compilador converte aplicação C# em aplicação MSIL (Microsoft Intermediate Language)
+    
+    Aplicação compilada MSIL = Aplicação .NET
+
+    Biblioteca em MSIL = .NET Framework (bibliotecas)
+
+    Máquina Emulada = CLR - Commom Language Runtime (máquina virtual)
---
+++ csharp-parte1-primeiros-passos/AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo. Projeto no visual studio");
+
+            Console.WriteLine("A execução finalizou. Tecle enter para finalizar...");
+
+            Console.ReadLine();
+        }
+    }
+}
